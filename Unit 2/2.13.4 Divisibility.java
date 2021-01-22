bbb8055b382dde6ee5fbe5a107a9400f7af45c85
Tanner1638 <tanner1638@gmail.com>
2021-01-21 19:53:42 -0600

    fixed error - Working
    CodeHS wasn't excepting != (not equal) operator for some reason... we just flipped it around :)

--- Unit 2/2.13.4 Divisibility.java
+++ Unit 2/2.13.4 Divisibility.java
@@ -9,24 +9,25 @@
 | review or compare your own work.                          |
 |_______________________Good_Luck___________________________|*/
 
-// NEEDS WORK NEEDS WORK NEEDS WORK NEEDS WORK NEEDS WORK \\
+
 public class Divisibility extends ConsoleProgram
 {
     public void run()
     {
         int dividend = readInt("Enter the dividend: ");
         int divisor = readInt("Enter the divisor: ");
-        if(divisor != 0){
-            System.out.println(dividend +" is not divisible by " + divisor + "!");
+
+        if(divisor == 0){
+            System.out.println(dividend + " is not divisible by " + divisor);
         }
-        else{
+        else if (dividend / divisor == (double) dividend / divisor){
             if(dividend / divisor == (double) dividend / divisor)
         {
             System.out.println(dividend + " is divisible by " + divisor + "!");
         }
         else
         {
-            System.out.println(dividend + " is not divisible by " + divisor);
+            System.out.println(dividend + " is divisible by " + divisor);
         }
         }
         
